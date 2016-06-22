3c3c2b71a34891b54497cefe2a16eee985d9f623
Keeky <hi@keeky.net>
2016-06-22 19:45:14 +0200

    Add counterData.js

---
+++ overwatch/counterData.js
@@ -0,0 +1,94 @@
+var couterData = {
+    offense: {
+        genji: {
+            strong: ['widowmaker', 'bastion', 'mercy', 'torbjorn'],
+            weak: ['pharah', 'symmetra', 'mei', 'tracer', 'zarya', 'lucio', 'winston', 'dva']
+        },
+        mccree: {
+            strong: ['reaper', 'reinhardt', 'pharah', 'winston', 'tracer', 'roadhog'],
+            weak: ['bastion', 'widowmaker', 'hanzo', 'zarya', 'torbjorn', 'genji']
+        },
+        pharah: {
+            strong: ['mei', 'junkrat', 'reaper', 'winston', 'bastion', 'lucio', 'symmetra', 'zarya', 'reinhardt'],
+            weak: ['roadhog', 'widowmaker', 'soldier76', 'mccree', 'bastion', 'genji', 'dva']
+        },
+        reaper: {
+            strong: ['bastion', 'mercy', 'mei', 'winston', 'widowmaker', 'symmetra', 'torbjorn'],
+            weak: ['mccree', 'pharah', 'lucio', 'junkrat']
+        },
+        soldier76: {
+            strong: ['pharah', 'tracer', 'mercy', 'zenyatta', 'torbjorn', 'roadhog'],
+            weak: ['bastion', 'reinhardt', 'zarya', 'roadhog']
+        },
+        tracer: {
+            strong: ['bastion', 'mercy', 'widowmaker', 'torbjorn', 'reinhardt', 'genji', 'zenyatta', 'hanzo'],
+            weak: ['mei', 'soldier76', 'junkrat', 'pharah', 'symmetra', 'mccree', 'reaper', 'roadhog', 'winston']
+        }
+    },
+    defense: {
+        bastion: {
+            strong: ['mccree', 'mercy', 'soldier76', 'zarya', 'pharah', 'winston', 'reinhardt'],
+            weak: ['tracer', 'reaper', 'symmetra', 'genji', 'pharah', 'junkrat', 'hanzo', 'widowmaker', 'mei', 'zenyatta']
+        },
+        hanzo: {
+            strong: ['bastion', 'mercy', 'zenyatta', 'torbjorn', 'roadhog', 'mccree'],
+            weak: ['reinhardt', 'genji', 'winston', 'tracer', 'widowmaker', 'dva']
+        },
+        junkrat: {
+            strong: ['bastion', 'mei', 'roadhog', 'tracer', 'reinhardt', 'dva', 'torbjorn', 'symmetra'],
+            weak: ['widowmaker', 'winston', 'reaper', 'pharah', 'mercy', 'soldier76', 'mccree']
+        },
+        mei: {
+            strong: ['genji', 'tracer', 'dva', 'winston', 'roadhog', 'bastion', 'lucio'],
+            weak: ['reaper', 'junkrat', 'pharah', 'zarya', 'mercy', 'mccree', 'widowmaker']
+        },
+        torbjorn: {
+            strong: ['genji', 'tracer', 'roadhog', 'zenyatta', 'symmetra', 'lucio'],
+            weak: ['reaper', 'widowmaker', 'dva', 'hanzo', 'junkrat', 'winston', 'reinhardt']
+        },
+        widowmaker: {
+            strong: ['zenyatta', 'pharah', 'mccree', 'hanzo', 'mercy', 'junkrat', 'zarya', 'mei', 'bastion'],
+            weak: ['reaper', 'tracer', 'genji', 'winston', 'dva']
+        }
+    },
+    tank: {
+        dva: {
+            strong: ['widowmaker', 'torbjorn', 'genji', 'mercy', 'pharah', 'reinhardt'],
+            weak: ['zenyatta', 'mei', 'zarya', 'roadhog', 'junkrat', 'mccree']
+        },
+        reinhardt: {
+            strong: ['soldier76', 'hanzo', 'torbjorn', 'widowmaker'],
+            weak: ['pharah', 'mccree', 'reaper', 'zenyatta', 'junkrat', 'tracer', 'symmetra', 'roadhog']
+        },
+        roadhog: {
+            strong: ['pharah', 'bastion', 'tracer', 'dva', 'mercy', 'lucio', 'reinhardt'],
+            weak: ['zenyatta', 'hanzo', 'torbjorn', 'junkrat', 'soldier76', 'mei', 'mccree']
+        },
+        winston: {
+            strong: ['symmetra', 'hanzo', 'genji', 'mercy', 'widowmaker', 'torbjorn'],
+            weak: ['mccree', 'reaper', 'pharah', 'lucio', 'zenyatta', 'reinhardt', 'mei', 'zarya', 'bastion']
+        },
+        zarya: {
+            strong: ['mei', 'winston', 'genji', 'lucio', 'zenyatta', 'soldier76', 'mccree', 'symmetra', 'dva'],
+            weak: ['widowmaker', 'pharah', 'bastion', 'mercy', 'roadhog', 'reaper']
+        }
+    },
+    support: {
+        lucio: {
+            strong: ['reaper', 'winston', 'genji', 'roadhog', 'dva'],
+            weak: ['pharah', 'zarya', 'mei', 'mccree']
+        },
+        mercy: {
+            strong: ['junkrat', 'zarya'],
+            weak: ['tracer', 'reaper', 'widowmaker', 'roadhog', 'soldier76', 'genji', 'mccree', 'hanzo', 'dva', 'bastion', 'winston']
+        },
+        symmetra: {
+            strong: ['bastion', 'reinhardt', 'genji', 'mercy', 'lucio'],
+            weak: ['pharah', 'reaper', 'junkrat', 'winston', 'zarya', 'roadhog', 'torbjorn']
+        },
+        zenyatta: {
+            strong: ['roadhog', 'dva', 'winston', 'zarya', 'reinhardt'],
+            weak: ['widowmaker', 'hanzo', 'torbjorn', 'tracer', 'soldier76']
+        },
+    }
+}
